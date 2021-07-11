4363a83e0fd93846d1623a8ccc039c29f411ffae
zhangbo <83893495+mlee128@users.noreply.github.com>
2021-07-11 15:57:28 +0900

    整理

--- src/main/two/app.kt
+++ src/main/two/app.kt
@@ -1,29 +1,17 @@
 package main.two
 
-
 fun main() {
-//    println("main two")
-//    main.one.other()
-//    vars(1,2,3,4,5,6)
-//    var sumLambda: (Int, Int) -> Int = { x, y -> x + y }
-//    println(sumLambda(1, 6))
-//    var a: String = "aaaa"
-//    var b = "a is $a"
-//    println(b)
-//    println("c is ${b.replace("a is ", "")}")
-/*    var age: String = "23"
-    println(age)*/
-//    println(age)
 
-/*    val ages1 = age?.toInt()
-    println(ages1)
-
-    val ages2 = age?.toInt() ?: -1
-    println(ages2)*/
 }
 
-fun vars(vararg v: Int) {
-    for (vt in v)
-        print(vt)
+enum class MyType(type : String) {
+    MY_FIRST("0"),
+    MY_SECOND("1");
+
+
+    companion object {
+
+    }
 }
+
 
